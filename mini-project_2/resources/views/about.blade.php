2487635b433fb98e3b359080676904ea8b3de63b
Chou <zhumuoy@gmail.com>
2026-01-17 19:02:58 +0700

    Commit

---
+++ mini-project_2/resources/views/about.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        .heading {
+            text-align: center;
+            background-color: royalblue;
+            color: white;
+            padding: 20px;
+        }
+        .main_body {
+            text-align: center;
+            padding: 20px;
+            background-color: palegoldenrod;
+            margin: 20px;
+        }
+        .footer {
+            text-align: center;
+            background-color: royalblue;
+            color: white;
+            padding: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <header class="heading">
+        <h1>⚜️ Welcome to the About Page! ⚜️</h1>
+    </header>
+
+    <main class="main_body">
+        <p>
+            My name is <strong>Sok Muoychou</strong> and this is my submission for mini-project <strong>2</strong> of Laravel. :D
+            <br><br>
+            I like reading and singing, nice to know you~ ^o^/
+        </p>
+    </main>
+
+    <footer class="footer">
+        <p>&copy; Footer, 2026</p>
+    </footer>
+</body>
+</html>
---
+++ mini-project_2/resources/views/welcome.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        .heading {
+            text-align: center;
+            background-color: palevioletred;
+            color: white;
+            padding: 20px;
+        }
+        .main_body {
+            text-align: center;
+            padding: 20px;
+            background-color: rgb(218, 255, 218);
+            margin: 20px;
+        }
+        .footer {
+            text-align: center;
+            background-color: palevioletred;
+            color: white;
+            padding: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <header class="heading">
+        <h1>🌸 Welcome to the Homepage! 🌸</h1>
+    </header>
+
+    <main class="main_body">
+        <p>
+            My name is <strong>Sok Muoychou</strong> and this is my submission for mini-project <strong>2</strong> of Laravel. :D
+            <br><br>
+            I like reading and singing, nice to know you~ ^o^/
+        </p>
+    </main>
+
+    <footer class="footer">
+        <p>&copy; Footer, 2026</p>
+    </footer>
+</body>
+</html>
